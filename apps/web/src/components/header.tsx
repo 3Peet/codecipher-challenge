ae8dd8925f2226bbfc7f70e171563604c0afc662
3Peet <mypeet096@gmail.com>
2025-07-09 20:37:12 +0700

    update: change api status styles to dot pulse

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,13 +1,25 @@
 import { caller } from "@/lib/trpc/server";
+import { cn } from "@/lib/utils";
 
 export default async function Header() {
 	const healthCheck = await caller.healthCheck();
 	return (
 		<div>
 			<div className="flex flex-row items-center gap-2 px-2 py-1">
-				<h2 className="font-medium">API Status:</h2>
-				<span className="text-muted-foreground text-sm">
-					{healthCheck ? "Connected" : "Disconnected"}
+				<span className="text-muted-foreground text-sm">API Status: </span>
+				<span className="relative flex size-2">
+					<span
+						className={cn(
+							"absolute inline-flex h-full w-full animate-ping rounded-full opacity-90",
+							healthCheck ? "bg-emerald-400" : "bg-red-300",
+						)}
+					/>
+					<span
+						className={cn(
+							"relative inline-flex size-2 rounded-full bg-emerald-500",
+							healthCheck ? "bg-emerald-500" : "bg-red-400",
+						)}
+					/>
 				</span>
 			</div>
 			<hr />
